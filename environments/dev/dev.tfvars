f5b3d5e35defc751ec598431b16909bd06597a16
rushimujumale <rushimujumale297@gmail.com>
2023-10-21 13:33:27 +0530

    1st commit

---
+++ environments/dev/dev.tfvars
@@ -0,0 +1,4 @@
+domain_name = "example-dev.com"
+comment = "Dev hosted zone"
+vpc_id = "vpc-dev-12345"
+is_private = true
---
+++ environments/dev/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  region = "us-east-1"  # Set the AWS region for the 'dev' environment
+  # Add other provider settings as needed
+}
